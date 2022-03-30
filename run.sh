c1b78f8848bf80132ee6f6f0c4fc025870751f9d
div72 <60045611+div72@users.noreply.github.com>
2022-03-30 13:18:40 +0300

    run: move config to a separate file

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-export BOT_ADMINS=
-export BOT_TRIGGER=@grcbountybot
-export GITHUB_APP_IDENTIFIER=
-export GITHUB_PRIVATE_KEY=$(cat <INSERT PRIVATE KEY PATH HERE>.pem)
-export WALLET_URL=
-export WALLET_RPC_USER=
-export WALLET_RPC_PASSWORD=
+if [ -f "$1" ]; then
+  source $1
+fi
+
 python3 -m grcbountybot
